c4dc9ad20331db8882346987a918fbc9428f5d16
Jack Guo <yiming7391@gmail.com>
2016-10-31 15:55:49 -0400

    basic script prototype, doesn't go for very long but at least it works.

--- Assets/Scripts/PathMaker.cs
+++ Assets/Scripts/PathMaker.cs
@@ -6,34 +6,36 @@ public class PathMaker : MonoBehaviour {
 	private int counter; 
 	public Transform floorPrefab;
 	public Transform pathmakerSpherePrefab;
+	static int tileCount;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		counter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (counter < 50) {
+		if (counter < 50 && tileCount <= 500) {
 			float randomNumber = Random.Range (0f, 1f);
+
 			if (randomNumber < 0.25f) {
-				transform.Rotate (Quaternion.Euler (0f, 90f, 0f));
-			} else {
-				if (randomNumber >= 0.25f && randomNumber <= 0.5f) {
-					transform.Rotate (Quaternion.Euler (0f, -90f, 0f));
-				} else {
-					if (randomNumber >= 0.99f) {
-						GameObject newClone = (GameObject)Instantiate (pathmakerSpherePrefab, 
-							                      new Vector3 (pathmakerSpherePrefab.transform.position.x, pathmakerSpherePrefab.transform.position.y, 0f));
-					}
-				}
+				transform.Rotate (0f, 90f, 0f);
+
+			} else if (randomNumber >= 0.25f && randomNumber <= 0.5f) {
+				transform.Rotate (0f, -90f, 0f);
+
+			} else if (randomNumber >= 0.99f) {
+				Instantiate (pathmakerSpherePrefab, transform.position, Quaternion.identity);
 			}
 
-			GameObject floorClone = (GameObject)Instantiate (floorPrefab, 
-				                        new Vector3 (pathmakerSpherePrefab.transform.position.x, pathmakerSpherePrefab.transform.position.y, 0f));
+			Instantiate (floorPrefab, transform.position, Quaternion.identity);
 
 			transform.Translate (0f, 0f, 5f);
 			counter++;
+			tileCount++;
+
 		} else {
 			Destroy (gameObject);
 		}
