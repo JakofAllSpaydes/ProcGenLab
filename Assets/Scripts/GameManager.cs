063cc78cebd4233d7645f17b5604cc64f912222e
Jack Guo <yiming7391@gmail.com>
2016-10-31 20:57:29 -0400

    Gameplay Commit - Generator working and can reset. No tile changes yet.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject Creator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.R)){
+			SceneManager.LoadScene(0);
+			Creator.GetComponent<PathMaker>().tileReset = 0;
+		}
+
+	}
+}
--- Assets/Scripts/PathMaker.cs
+++ Assets/Scripts/PathMaker.cs
@@ -1,32 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class PathMaker : MonoBehaviour {
 	
 	private int counter; 
 	public Transform floorPrefab;
 	public Transform pathmakerSpherePrefab;
 	static int tileCount;
+	public int tileReset;
+	public int lifeSpan;
+	public float turn1;
+	public float turn2;
+	public float turn3;
 
 
 	// Use this for initialization
 	void Start () {
 		counter = 0;
+		lifeSpan = (Random.Range(50, 200));
+		turn1 = (Random.Range(0.15f, 0.3f));
+		turn2 = (Random.Range (0.3f, 0.6f));
+		turn3 = (Random.Range (0.6f, 0.85f));
+
+		tileCount = tileReset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (counter < 50 && tileCount <= 500) {
+
+		tileReset = tileCount;
+
+		if (counter < lifeSpan && tileCount <= 500) {
 			float randomNumber = Random.Range (0f, 1f);
 
-			if (randomNumber < 0.25f) {
+			if (randomNumber < turn1) {
 				transform.Rotate (0f, 90f, 0f);
 
-			} else if (randomNumber >= 0.25f && randomNumber <= 0.5f) {
+			} else if (randomNumber >= turn2 && randomNumber <= turn3) {
 				transform.Rotate (0f, -90f, 0f);
 
-			} else if (randomNumber >= 0.99f) {
+			} else if (randomNumber >= turn3) {
 				Instantiate (pathmakerSpherePrefab, transform.position, Quaternion.identity);
 			}
 
